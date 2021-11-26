0bbc004c6a5d89056d96e486e0f645f98f8f8dc5
Frank Santolin <frank@cadha.com>
2021-11-26 13:54:02 +0000

    improvements

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const chalk = require('chalk');
+const boxen = require('boxen');
 
-var app = express();
+const app = express();
 
-var bootstrapPath = path.resolve(__dirname, 'bootstrap/dist/');
-var welcomePath = path.resolve(__dirname, 'welcome/dist/');
-var musicPath = path.resolve(__dirname, 'music/build/');
-
-app.use('/', express.static(bootstrapPath));
+const bootstrapPath = path.resolve(__dirname, 'bootstrap/dist/');
+const welcomePath = path.resolve(__dirname, 'welcome/dist/');
+const musicPath = path.resolve(__dirname, 'music/build/');
 
 app.use('/mfe/welcome/', express.static(welcomePath));
-
 app.use('/mfe/music/', express.static(musicPath));
+app.use('/', express.static(bootstrapPath));
 
 app.get("*", function(request, response) {
 	response.status(404).send('Page not found!');
 });
 
 app.listen(3000, function() {
-	console.log('Express server started on port 3000');
+	console.log(
+		boxen(
+			chalk.bold('Web Server running at: ') + chalk.underline.blue('http://localhost:3000')
+			+ chalk.bold('\n\n' + chalk.cyan('Welcome ') + 'Micro Frontend: ') + chalk.underline.blue('http://localhost:3000/hello')
+			+ chalk.bold('\n' + chalk.cyan('Music ') + 'Micro Frontend: ') + chalk.underline.blue('http://localhost:3000/play'),
+			{ padding: 1, borderColor: 'green' }
+		)
+	);
 });
