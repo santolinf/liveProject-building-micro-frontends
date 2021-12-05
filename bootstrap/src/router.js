147830014c8f9eeb088c947147425d28bba58f3f
Frank Santolin <frank@cadha.com>
2021-12-05 18:15:03 +0000

    improvements

--- bootstrap/src/router.js
+++ bootstrap/src/router.js
@@ -4,29 +4,29 @@ import download from './download';
 
 const navigationHistory = []; // keep track of mfe mounted
 
-function loadMicroFrontend(path) {
-  const microFrontendName = mapPathnameToMicroFrontendName(path);
-
-  if (microFrontendName) {
-    download(microFrontendName)
-      .then(microFrontend => mountMicroFrontendToCurrentDocument(microFrontendName, microFrontend))
-      .catch(error => new Error('Cannot load Micro Frontend: ' + error));
-  } else {
-    throw new Error('Micro Frontend not found for path: ' + window.location.pathname);
-  }
+function loadMicroFrontend(microFrontendName) {
+  download(microFrontendName)
+    .then(microFrontend => mountMicroFrontendToCurrentDocument(microFrontendName, microFrontend))
+    .catch(error => new Error('Cannot mount Micro Frontend: ' + error));
 }
 
 function navigateTo(pathname) {
+  const microFrontendName = mapPathnameToMicroFrontendName(pathname);
+
+  if (!microFrontendName) {
+    throw new Error('Micro Frontend not found for path: ' + pathname);
+  }
+
   // if not the first mfe, unmount the nodes
   if (navigationHistory.length > 0) {
-    unmountMicroFrontendFromDocument();
+    unmountMicroFrontendFromDocument(microFrontendName);
   }
 
   navigationHistory.push(pathname);
   // update browser url
   window.history.pushState({}, '', pathname);
 
-  loadMicroFrontend(pathname);
+  loadMicroFrontend(microFrontendName);
 }
 
 export default {
