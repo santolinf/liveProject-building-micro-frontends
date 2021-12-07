f7e55c4801e1c3d68e343774d1213cc754854bb9
Frank Santolin <frank@cadha.com>
2021-12-07 08:31:03 +0000

    improvements

--- bootstrap/src/auth.js
+++ bootstrap/src/auth.js
@@ -1,13 +1,39 @@
 
+const TOKEN_KEY = 'boostrap_auth_token';
+
+let isUserAuthenticated = false;
+
 function setToken(token) {
-  window.localStorage.setItem('token', token);
+  if (!token) {
+    window.localStorage.removeItem(TOKEN_KEY);
+    return;
+  }
+
+  window.localStorage.setItem(TOKEN_KEY, token);
+  isUserAuthenticated = true;
 }
 
 function getToken() {
-  return window.localStorage.getItem('token');
+  return window.localStorage.getItem(TOKEN_KEY);
 }
 
-export default {
+function validateToken() {
+  const token = getToken();
+  if (!token) {
+    return Promise.resolve(false);
+  }
+
+  return fetch(process.env.TOKEN_VALIDATE_URL, {
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${token}` }
+  })
+    .then(response => isUserAuthenticated = response.ok)
+    .catch(() => isUserAuthenticated = false);
+}
+
+export {
   setToken,
-  getToken
+  getToken,
+  validateToken,
+  isUserAuthenticated
 }
--- bootstrap/src/router.js
+++ bootstrap/src/router.js
@@ -1,16 +1,12 @@
 import { mountMicroFrontendToCurrentDocument, unmountMicroFrontendFromDocument } from './mount';
 import { mapPathnameToMicroFrontend, welcomeMicroFrontend, musicMicroFrontend } from './config';
+import { isUserAuthenticated }  from './auth';
 import download from './download';
 
-let userAuthenticated = false;
 const navigationHistory = []; // keep track of mfe mounted
 
-function init(authenticated = false) {
-  userAuthenticated = authenticated;
-}
-
 function determineMicroFrontendToGoNext(pathname) {
-  if (userAuthenticated) {  // ignore requested pathname
+  if (isUserAuthenticated) {  // ignore requested pathname
     console.log('user is authenticated, go to music frontend');
     return musicMicroFrontend;
   }
@@ -33,14 +29,14 @@ function determineMicroFrontendToGoNext(pathname) {
 function loadMicroFrontend(microFrontendName) {
   download(microFrontendName)
     .then(microFrontend => mountMicroFrontendToCurrentDocument(microFrontendName, microFrontend))
-    .catch(error => new Error('Cannot mount Micro Frontend: ' + error));
+    .catch(error => new Error('Cannot mount frontend: ' + error));
 }
 
 function navigateTo(pathname) {
   const microFrontend = determineMicroFrontendToGoNext(pathname);
 
   if (!microFrontend.name) {
-    throw new Error('Micro Frontend not found for path: ' + microFrontend.pathname);
+    throw new Error('Frontend not found for path: ' + microFrontend.pathname);
   }
 
   // if not the first mfe, unmount the nodes
@@ -56,7 +52,6 @@ function navigateTo(pathname) {
   loadMicroFrontend(microFrontend.name);
 }
 
-export default {
-  init,
+export {
   navigateTo
 }
